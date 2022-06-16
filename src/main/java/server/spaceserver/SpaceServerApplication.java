da3d92bd7282f8f33fae11a06f8e2dfbddd2f174
cancadua <cancadua@protonmail.com>
2022-06-16 17:19:40 +0200

    Initialization

---
+++ src/main/java/server/spaceserver/SpaceServerApplication.java
@@ -0,0 +1,13 @@
+package server.spaceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceServerApplication.class, args);
+    }
+
+}
